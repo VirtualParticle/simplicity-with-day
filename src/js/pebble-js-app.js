6bae7272d95ea9f815263725dce449d7b0cd6b94
Mark Vadeika <markvadeika@gmail.com>
2023-07-07 18:16:53 -0400

    reformat

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -14,55 +14,55 @@ var STORM = 12;
 var NA = 13;
 
 var imageId = {
-    0 : STORM, //tornado
-    1 : STORM, //tropical storm
-    2 : STORM, //hurricane
-    3 : STORM, //severe thunderstorms
-    4 : STORM, //thunderstorms
-    5 : HAIL, //mixed rain and snow
-    6 : HAIL, //mixed rain and sleet
-    7 : HAIL, //mixed snow and sleet
-    8 : HAIL, //freezing drizzle
-    9 : RAIN, //drizzle
-    10 : HAIL, //freezing rain
-    11 : RAIN, //showers
-    12 : RAIN, //showers
-    13 : SNOW, //snow flurries
-    14 : SNOW, //light snow showers
-    15 : SNOW, //blowing snow
-    16 : SNOW, //snow
-    17 : HAIL, //hail
-    18 : HAIL, //sleet
-    19 : HAZE, //dust
-    20 : HAZE, //foggy
-    21 : HAZE, //haze
-    22 : HAZE, //smoky
-    23 : WINDY, //blustery
-    24 : WINDY, //windy
-    25 : COLD, //cold
-    26 : CLOUDY, //cloudy
-    27 : CLOUDY, //mostly cloudy (night)
-    28 : CLOUDY, //mostly cloudy (day)
-    29 : PARTLY_CLOUDY_NIGHT, //partly cloudy (night)
-    30 : PARTLY_CLOUDY_DAY, //partly cloudy (day)
-    31 : CLEAR_NIGHT, //clear (night)
-    32 : CLEAR_DAY, //sunny
-    33 : CLEAR_NIGHT, //fair (night)
-    34 : CLEAR_DAY, //fair (day)
-    35 : HAIL, //mixed rain and hail
-    36 : CLEAR_DAY, //hot
-    37 : STORM, //isolated thunderstorms
-    38 : STORM, //scattered thunderstorms
-    39 : STORM, //scattered thunderstorms
-    40 : STORM, //scattered showers
-    41 : SNOW, //heavy snow
-    42 : SNOW, //scattered snow showers
-    43 : SNOW, //heavy snow
-    44 : CLOUD, //partly cloudy
-    45 : STORM, //thundershowers
-    46 : SNOW, //snow showers
-    47 : STORM, //isolated thundershowers
-    3200 : NA, //not available
+    0: STORM, //tornado
+    1: STORM, //tropical storm
+    2: STORM, //hurricane
+    3: STORM, //severe thunderstorms
+    4: STORM, //thunderstorms
+    5: HAIL, //mixed rain and snow
+    6: HAIL, //mixed rain and sleet
+    7: HAIL, //mixed snow and sleet
+    8: HAIL, //freezing drizzle
+    9: RAIN, //drizzle
+    10: HAIL, //freezing rain
+    11: RAIN, //showers
+    12: RAIN, //showers
+    13: SNOW, //snow flurries
+    14: SNOW, //light snow showers
+    15: SNOW, //blowing snow
+    16: SNOW, //snow
+    17: HAIL, //hail
+    18: HAIL, //sleet
+    19: HAZE, //dust
+    20: HAZE, //foggy
+    21: HAZE, //haze
+    22: HAZE, //smoky
+    23: WINDY, //blustery
+    24: WINDY, //windy
+    25: COLD, //cold
+    26: CLOUDY, //cloudy
+    27: CLOUDY, //mostly cloudy (night)
+    28: CLOUDY, //mostly cloudy (day)
+    29: PARTLY_CLOUDY_NIGHT, //partly cloudy (night)
+    30: PARTLY_CLOUDY_DAY, //partly cloudy (day)
+    31: CLEAR_NIGHT, //clear (night)
+    32: CLEAR_DAY, //sunny
+    33: CLEAR_NIGHT, //fair (night)
+    34: CLEAR_DAY, //fair (day)
+    35: HAIL, //mixed rain and hail
+    36: CLEAR_DAY, //hot
+    37: STORM, //isolated thunderstorms
+    38: STORM, //scattered thunderstorms
+    39: STORM, //scattered thunderstorms
+    40: STORM, //scattered showers
+    41: SNOW, //heavy snow
+    42: SNOW, //scattered snow showers
+    43: SNOW, //heavy snow
+    44: CLOUD, //partly cloudy
+    45: STORM, //thundershowers
+    46: SNOW, //snow showers
+    47: STORM, //isolated thundershowers
+    3200: NA, //not available
 };
 
 var imageMapping = {
@@ -145,10 +145,12 @@ var imageMapping = {
 
 var options = JSON.parse(localStorage.getItem('options'));
 //console.log('read options: ' + JSON.stringify(options));
-if (options === null) options = { "use_gps" : "true",
-    "location" : "",
-    "units" : "fahrenheit",
-    "invert_color" : "false"};
+if (options === null) options = {
+    "use_gps": "true",
+    "location": "",
+    "units": "fahrenheit",
+    "invert_color": "false"
+};
 
 function getWeatherFromLatLong(latitude, longitude) {
 
@@ -156,7 +158,7 @@ function getWeatherFromLatLong(latitude, longitude) {
 
     var url = "https://api.weather.gov/points/" + latitude + "%2C" + longitude + "/stations";
 
-    req.onload = function(e) {
+    req.onload = function (e) {
 
         if (req.readyState == 4) {
             if (req.status == 200) {
@@ -165,12 +167,12 @@ function getWeatherFromLatLong(latitude, longitude) {
                     getWeatherFromStationUrl(response.observationStations[0])
                 }
             } else {
-		    test("LatLong");
+                test("LatLong");
                 console.log("Error LatLong");
             }
         } else {
-		test("LtLgSt");
-	}
+            test("LtLgSt");
+        }
     };
 
     req.open("GET", url);
@@ -180,10 +182,10 @@ function getWeatherFromLatLong(latitude, longitude) {
 }
 
 function test(temp) {
-	Pebble.sendAppMessage({
-		"icon": 7,
-		"temperature": temp
-	});
+    Pebble.sendAppMessage({
+        "icon": 7,
+        "temperature": temp
+    });
 }
 
 function getWeatherFromStationUrl(url) {
@@ -216,14 +218,14 @@ function getWeatherFromStationUrl(url) {
                     var icon = icons[iconName];
 
                     Pebble.sendAppMessage({
-                        "icon" : icon,
-                        "temperature" : Math.round(tempValue) + (celsius ? "\u00B0C" : "\u00B0F"),
-                        "invert_color" : (options["invert_color"] == "true" ? 1 : 0)
+                        "icon": icon,
+                        "temperature": Math.round(tempValue) + (celsius ? "\u00B0C" : "\u00B0F"),
+                        "invert_color": (options["invert_color"] == "true" ? 1 : 0)
                     });
 
                 }
             } else {
-		    test("station");
+                test("station");
                 console.log("Error StationUrl");
             }
         }
@@ -243,7 +245,7 @@ function getWeatherFromLocation(location_name) {
     var url = "http://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json";
     var req = new XMLHttpRequest();
     req.open('GET', url, true);
-    req.onload = function(e) {
+    req.onload = function (e) {
         if (req.readyState == 4) {
             if (req.status == 200) {
                 // console.log(req.responseText);
@@ -269,7 +271,7 @@ function getWeatherFromWoeid(woeid) {
     var response;
     var req = new XMLHttpRequest();
     req.open('GET', url, true);
-    req.onload = function(e) {
+    req.onload = function (e) {
         if (req.readyState == 4) {
             if (req.status == 200) {
                 response = JSON.parse(req.responseText);
@@ -281,9 +283,9 @@ function getWeatherFromWoeid(woeid) {
                     // console.log("icon " + icon);
                     // console.log("condition " + condition.text);
                     Pebble.sendAppMessage({
-                        "icon" : icon,
-                        "temperature" : temperature,
-                        "invert_color" : (options["invert_color"] == "true" ? 1 : 0),
+                        "icon": icon,
+                        "temperature": temperature,
+                        "invert_color": (options["invert_color"] == "true" ? 1 : 0),
                     });
                 }
             } else {
@@ -314,12 +316,12 @@ function locationSuccess(pos) {
 function locationError(err) {
     console.warn('location error (' + err.code + '): ' + err.message);
     Pebble.sendAppMessage({
-        "icon":11,
-        "temperature":""
+        "icon": 11,
+        "temperature": ""
     });
 }
 
-Pebble.addEventListener('showConfiguration', function(e) {
+Pebble.addEventListener('showConfiguration', function (e) {
     var uri = 'http://tallerthenyou.github.io/simplicity-with-day/configuration.html?' +
         'use_gps=' + encodeURIComponent(options['use_gps']) +
         '&location=' + encodeURIComponent(options['location']) +
@@ -330,7 +332,7 @@ Pebble.addEventListener('showConfiguration', function(e) {
     Pebble.openURL(uri);
 });
 
-Pebble.addEventListener('webviewclosed', function(e) {
+Pebble.addEventListener('webviewclosed', function (e) {
     if (e.response) {
         options = JSON.parse(decodeURIComponent(e.response));
         localStorage.setItem('options', JSON.stringify(options));
@@ -341,10 +343,10 @@ Pebble.addEventListener('webviewclosed', function(e) {
     }
 });
 
-Pebble.addEventListener("ready", function(e) {
+Pebble.addEventListener("ready", function (e) {
     //console.log("connect!" + e.ready);
     updateWeather();
-    setInterval(function() {
+    setInterval(function () {
         //console.log("timer fired");
         updateWeather();
     }, 1800000); // 30 minutes
